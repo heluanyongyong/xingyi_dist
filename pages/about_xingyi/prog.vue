cb7ab165820a76cceb9222faa1f02e10d00f05d4
suyan <936737951@qq.com>
2018-11-13 10:12:04 +0800

    11.13 suyan

--- pages/about_xingyi/prog.vue
+++ pages/about_xingyi/prog.vue
@@ -1,50 +1,52 @@
 <template>
 	<div class="flex2 clearfix bgColor box">
 		<about></about>
-		<ul class="grey font18 div1 tr ul_left wrBorder1 clearfix fl mg3">
-			<li>2018-5</li>
-			<li>2018-2</li>
-			<li>2017-7</li>
-			<li>2017-2</li>
-			<li>2016-7</li>
-		</ul>
-		<ul class="ul_right tl fr pt3">
-			<li class="clearfix">
-				<div>
-					<h3>伊步慈善，一个有爱的健康倡导者</h3>
-					<p>5月22日，广东伊步网络科技有限公司与深圳市慈善会成立的深圳市慈善会伊步慈善基金正式挂牌运营了</p>
-				</div>
-				<img src="/about_img/one.png" alt="图片加载中..">
-			</li>
-			<li class="clearfix">
-				<div>
-					<h3>2018年年会</h3>
-					<p>大步跨过了2017年，我们用汗水耕耘，去描绘伊步更好的未来</p>
-				</div>
-				<img src="/about_img/two.png" alt="图片加载中..">
-			</li>
-			<li class="clearfix">
-				<div>
-					<h3>伊步三周年庆</h3>
-					<p>这一路辛勤汗水浇灌成功的幼苗，智慧的力量引来蜂蝶飞舞，齐心协力共同奋斗，迎接伊步三周年庆典</p>
-				</div>
-				<img src="/about_img/three.png" alt="图片加载中..">
-			</li>
-			<li class="clearfix">
-				<div>
-					<h3>2017年年会</h3>
-					<p>伴随着欢乐的乐曲，伊步迎接2017年的年会</p>
-				</div>
-				<img src="/about_img/four.png" alt="图片加载中..">
-			</li>
-			<li class="clearfix">
-				<div>
-					<h3>伊步两周年庆</h3>
-					<p>走过了收获的秋天，寒冷的冬天，希望的春天，我们迎来了充满期待的夏天，我们满情壮志的迎来了公司二周年庆典</p>
-				</div>
-				<img src="/about_img/five.png" alt="图片加载中..">
-			</li>
-		</ul>
+		<div class="container clearfix ptb3">
+			<ul class="grey font36 ul_left wrBorder1 clearfix fl">
+				<li>2018-5</li>
+				<li>2018-2</li>
+				<li>2017-7</li>
+				<li>2017-2</li>
+				<li>2016-7</li>
+			</ul>
+			<ul class="ul_right tl fr">
+				<li class="clearfix">
+					<div>
+						<div>伊步慈善，一个有爱的健康倡导者</div>
+						<p>5月22日，广东伊步网络科技有限公司与深圳市慈善会成立的深圳市慈善会伊步慈善基金正式挂牌运营了</p>
+					</div>
+					<img src="/about_img/one.png" alt="图片加载中..">
+				</li>
+				<li class="clearfix">
+					<div>
+						<div>2018年年会</div>
+						<p>大步跨过了2017年，我们用汗水耕耘，去描绘伊步更好的未来</p>
+					</div>
+					<img src="/about_img/two.png" alt="图片加载中..">
+				</li>
+				<li class="clearfix">
+					<div>
+						<div>伊步三周年庆</div>
+						<p>这一路辛勤汗水浇灌成功的幼苗，智慧的力量引来蜂蝶飞舞，齐心协力共同奋斗，迎接伊步三周年庆典</p>
+					</div>
+					<img src="/about_img/three.png" alt="图片加载中..">
+				</li>
+				<li class="clearfix">
+					<div>
+						<div>2017年年会</div>
+						<p>伴随着欢乐的乐曲，伊步迎接2017年的年会</p>
+					</div>
+					<img src="/about_img/four.png" alt="图片加载中..">
+				</li>
+				<li class="clearfix">
+					<div>
+						<div>伊步两周年庆</div>
+						<p>走过了收获的秋天，寒冷的冬天，希望的春天，我们迎来了充满期待的夏天，我们满情壮志的迎来了公司二周年庆典</p>
+					</div>
+					<img src="/about_img/five.png" alt="图片加载中..">
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
@@ -64,39 +66,39 @@ export default {
 </script>
 
 <style scoped>
-.ul_right{
-	width: 70%;
-}
-.ul_left{
-	width: 30%;
-}
 .ul_left li{
     border-bottom: 1px solid #E0E0E0;
-    width: 30%;
-    padding: 10% 5%;
-    margin: 3% 0 3% 65%;
     text-align: center;
+    width: 170px;
+    margin: 20px 30px 20px 0;
+    padding: 70px 0;
+}
+.ul_right{
+	width: 886px;
+	height: 173px;
 }
 .ul_right li{
-	width: 60%;
     background-color: white;
-    margin-bottom: 1rem;
-    padding: 1rem;
+    margin-bottom: 50px;
+    padding: 20px;
 }
 .ul_right div{
-	width: 57%;
 	float: left;
+	width: 538px;
 }
 .ul_right img{
 	float: right;
-	width: 20%;
+	width: 183px;
 }
 .ul_right p{
-	margin-top: 1rem;
-	font-size: 0.9rem;
+	font-size: 18px;
+	margin-top: 30px;
 	color: #999999;
 }
 .mg3{
 	margin-top: 3rem;
+}
+.ul_right div{
+	font-size: 32px;
 }
 </style>
--- pages/download_center/index.vue
+++ pages/download_center/index.vue
@@ -1,46 +1,48 @@
 <template>
   <div>
     <div  class="download_bgimg1">
-      <div class="mg50 clearfix pt5">
+      <div class="clearfix pt5 container">
         <div class="tc fl w30 rel">
-          <img src="../../static/down_img/box.png" alt="图片加载中..">
-          <img src="../../static/down_img/star.png" alt="图片加载中.." class="abs">
+          <img src="/down_img/box.png" alt="图片加载中..">
+          <img src="/down_img/star.png" alt="图片加载中.." class="abs">
         </div>
-        <div class="tl white fr w70">
-          <p class="mb5">
-            <img src="../../static/down_img/text1.png" alt="文字加载中.." class="w50">
+        <div class="tl white fr">
+          <p class="mb125">
+            <img src="/down_img/text1.png" alt="文字加载中.." class="w50">
           </p>
           <p class="bgYellow black borR20 tc mar0">
-            <img src="../../static/down_img/download.png"> <a href="javascript:" id="a">下载星移盒子APP</a>
+            <img src="/down_img/download.png"> <a href="javascript:" id="a">下载星移盒子APP</a>
           </p>
         </div>
       </div>
     </div>
     <div class="download_bgimg2">
-        <div class="mg50 clearfix pd5">
-          <div class="w30 fl">
-            <p><img src="../../static/down_img/text2.png" alt="文字加载中.."></p>
-            <p class="grey mt3"><a href="javascript:">带你发现更大的游戏世界</a></p>
+        <div class="clearfix container download_div1">
+          <div class="fl">
+            <p><img src="/down_img/text2.png" alt="文字加载中.."></p>
+            <p class="grey mt45 font24"><a href="javascript:">带你发现更大的游戏世界</a></p>
           </div>
-          <div class="fr w70"><img src="../../static/down_img/img8.png" alt="文字加载中.."></div>
+          <img src="/down_img/img8.png" alt="文字加载中.." class="fr">
         </div>
     </div>
-    <div class="bgBlack tc pd5">
-      <p><img src="../../static/down_img/text3.png" alt="文字加载中.." class="mb5"></p>
-      <ul class="clearfix">
-        <li>
-          <p><a href="javascript:"><img src="../../static/down_img/strategy.png" alt="图片加载中.."></a></p>
-          <p><a href="javascript:">游戏攻略</a></p>
-        </li>
-        <li>
-          <p><a href="javascript:"><img src="../../static/down_img/interflow.png" alt="图片加载中.."></a></p>
-          <p><a href="javascript:">游戏社交</a></p>
-        </li>
-        <li>
-          <p><a href="javascript:"><img src="../../static/down_img/consult.png" alt="图片加载中.."></a></p>
-          <p><a href="javascript:">游戏咨询</a></p>
-        </li>
-      </ul>
+    <div class="bgBlack tc">
+      <div class="container pd175">
+        <p><img src="/down_img/text3.png" alt="文字加载中.." class="mb125"></p>
+        <ul class="clearfix">
+          <li>
+            <p><a href="javascript:"><img src="/down_img/strategy.png" alt="图片加载中.."></a></p>
+            <p><a href="javascript:">游戏攻略</a></p>
+          </li>
+          <li>
+            <p><a href="javascript:"><img src="/down_img/interflow.png" alt="图片加载中.."></a></p>
+            <p><a href="javascript:">游戏社交</a></p>
+          </li>
+          <li>
+            <p><a href="javascript:"><img src="/down_img/consult.png" alt="图片加载中.."></a></p>
+            <p><a href="javascript:">游戏咨询</a></p>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -71,11 +73,12 @@ export default {
   width: 180px;
   line-height: 40px;
 }
-body .font5{
-  font-size: 5rem;
+.download_div1{
+  padding: 136px 0;
 }
-body .mar0{margin: 0}
-body .pd0{padding: 0}
+body .mar0{
+  margin: 0;
+}
 .abs{
   position: absolute;
   top: -32%;
@@ -84,36 +87,33 @@ body .pd0{padding: 0}
 .rel{
   position: relative;
 }
-.w30{
-  width: 25%
-}
-.w70{
-  width: 60%;
-}
 .pt5{
-  padding-top: 9rem
+  padding-top: 145px;
 }
-.pd5{
-  padding: 5rem 0;
+.mb125{
+  margin-bottom: 125px;
 }
-.mb5{
-  margin-bottom: 5%;
-}
-a{
+li a{
   color: #fff;
+  font-size: 30px;
 }
 li{
   float: left;
-  margin-left: 20%;
-  overflow: hidden;
+  margin-left: 123px;
 }
 .bgYellow{
   background-color: #FDD300
 }
-.mt3{
-  margin-top: 3rem;
+.mt45{
+  margin-top: 45px;
 }
 #a{
   color: black
+}
+.font24 a{
+  color: #999
+}
+.pd175{
+  padding: 175px 0;
 }
 </style>
